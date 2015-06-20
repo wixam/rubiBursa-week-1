0e30dc475b6d595108adc04be66ad9c8a7cae632
wixam <infomaxg@gmail.com>
2015-06-20 23:23:09 +0300

    check logic operator

--- un-1.rb
+++ un-1.rb
@@ -5,7 +5,7 @@ a = 1
 res =if a > 5
 	"#{a} is bigger then 5"
 	 elsif a < 5
-		"#{a} is bigger then 5"
+		"#{a} is smaller then 5"
 	 else
 		 "#{a} is equal 5"
 	 end
@@ -13,4 +13,9 @@ puts res
 
 
 
-		
+puts a = nil || false
+
+puts a = nil || 10
+
+puts b = a && 10
+  
